c0e290025e8771b43d62d6fd334c200ac9354072
janemba <janemba@nanotek.info>
2015-10-24 23:56:35 +0200

    add nyaa tracker

--- downloader/__meta__.py
+++ downloader/__meta__.py
@@ -4,7 +4,7 @@
 __app_name__    = 'downloader'
 __author__      = 'janemba'
 __email__       = 'cap.janemba@gmail.com'
-__version__     = '1.1.6'
+__version__     = '1.1.7'
 __description__ = 'Search and download torrent from multiple private trackers'
 __maintainer__  = '[janemba <cap.janemba@gmail.com>]'
 __url__         = 'http://none'
