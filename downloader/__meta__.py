2b202bac1c5cab6ccf764fe34e437d1f2a6a0e14
janemba <janemba@nanotek.info>
2015-11-02 22:57:42 +0100

    versionning

--- downloader/__meta__.py
+++ downloader/__meta__.py
@@ -1,10 +1,10 @@
 
-# -*- coding: utf-8 -*-
+1;2802;0c# -*- coding: utf-8 -*-
 
 __app_name__    = 'downloader'
 __author__      = 'janemba'
 __email__       = 'cap.janemba@gmail.com'
-__version__     = '1.3.8'
+__version__     = '1.3.9'
 __description__ = 'Search and download torrent from multiple private trackers'
 __maintainer__  = '[janemba <cap.janemba@gmail.com>]'
 __url__         = 'http://none'
