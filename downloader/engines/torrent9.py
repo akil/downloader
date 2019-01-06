e7ef0249cbe6e315a37e8674d6aded55753a7551
akil <akil@nanotek.info>
2019-01-06 19:40:01 +0400

    FIX: change in torrent9 engine

--- downloader/engines/torrent9.py
+++ downloader/engines/torrent9.py
@@ -32,7 +32,7 @@ class Torrent9(engine.Engine):
             torurl  = urlparse.urljoin(self._config['url-root'], rellink)
             torpage = self._session.get(torurl, verify=False)
             tortree = etree.HTML(torpage.content)
-            torlink = tortree.xpath('//a[@class="btn btn-danger download"]')[1]
+            torlink = tortree.xpath('//a[@class="btn btn-danger download"]')[0]
 
             url = urlparse.urljoin(self._config['url-root'], torlink.get('href'))
 
