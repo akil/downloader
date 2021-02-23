b1bc4c33acfae51f1a2bb6208f1dfc370339d070
Akil <akil@nanotek.info>
2021-02-23 17:28:01 +0400

    fix inconsistent HTML

--- downloader/__meta__.py
+++ downloader/__meta__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
-__version__     = '1.9.18'
-__project__     = 'downloader'
-__description__ = 'multiple torrent-engine downloader'
-__epilog__      = '''
+__version__ = "1.9.19"
+__project__ = "downloader"
+__description__ = "multiple torrent-engine downloader"
+__epilog__ = """
   $ ./downloader --help
-'''
-__author__      = "akil"
-__copyright__   = "(_)_)======D 2017"
-__license__     = "WTFPL"
-__maintainer__  = "akil"
-__email__       = "akil@nanotek.info"
-__status__      = "Production"
+"""
+__author__ = "akil"
+__copyright__ = "(_)_)======D 2017"
+__license__ = "WTFPL"
+__maintainer__ = "akil"
+__email__ = "akil@nanotek.info"
+__status__ = "Production"
--- downloader/engines/oxtorrent.py
+++ downloader/engines/oxtorrent.py
@@ -1,65 +1,70 @@
 # -*- coding: utf-8 -*-
 
 import cfscrape
+import lxml.html.clean
+import requests
 import urlparse
 from lxml import etree
-import requests
-
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 import os
+
 import engine
 
 
 class Oxtorrent(engine.Engine):
-
     def __init__(self):
 
-        self._name    = 'oxtorrent'
+        self._name = "oxtorrent"
         self._session = None
 
     def _results(self, pagetree):
 
         res = list()
-        for item in pagetree.xpath('//tbody/tr'):
-            cells = item.xpath('td')
+        for item in pagetree.xpath("//tbody/tr"):
+            cells = item.xpath("td")
 
-            if len(cells) != 4: continue
+            if len(cells) != 4:
+                continue
 
-            link  = cells[0].xpath('//div/a').pop()
+            link = cells[0].xpath("//div/a").pop()
 
-            seed     = int(cells[2].xpath('text()').pop().strip())
+            seed = int(cells[2].xpath("text()").pop().strip())
             filename = link.text
-            torlink  = urlparse.urljoin(self._config['url-root'], link.get('href'))
+            torlink = urlparse.urljoin(self._config["url-root"], link.get("href"))
 
-            query   = self._session.get(torlink, verify=False)
+            query = self._session.get(torlink, verify=False)
             tortree = etree.HTML(query.content)
             query.close()
 
             dlclass = tortree.xpath('//div[@class="btn-magnet"]').pop()
-            dllink  = dlclass.xpath('a').pop().get('href')
+            dllink = dlclass.xpath("a").pop().get("href")
 
-            res.append({
-                'filename' : filename,
-                'url'      : urlparse.urljoin(self.url(), dllink),
-                'seed'     : seed
-            })
+            res.append(
+                {
+                    "filename": filename,
+                    "url": urlparse.urljoin(self.url(), dllink),
+                    "seed": seed,
+                }
+            )
 
         return res
 
     def _search(self, filename):
 
-        query   = filename.replace('.', ' ')
-        payload = {'torrentSearch' : query}
+        query = filename.replace(".", " ")
+        payload = {"torrentSearch": query}
 
-        url_search = self._config['url-root']
+        url_search = self._config["url-root"]
 
-        p = self._session.post(url_search, data = payload, verify=False)
+        p = self._session.post(url_search, data=payload, verify=False)
         p.close()
 
-        tree  = etree.HTML(p.content)
-        res   = self._results(tree)
+        page = lxml.html.clean.clean_html(p.content)
+        tree = etree.HTML(page)
+        res = self._results(tree)
 
         return res
 
@@ -69,15 +74,17 @@ class Oxtorrent(engine.Engine):
 
     def url(self):
 
-        return self._config['url-root']
+        return self._config["url-root"]
 
     def get(self, filename, config):
 
-        self._config  = config
+        self._config = config
         self._session = cfscrape.create_scraper()
 
         self._session.headers.update(
-            {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'}
+            {
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0"
+            }
         )
 
         return self._search(filename), self._session
